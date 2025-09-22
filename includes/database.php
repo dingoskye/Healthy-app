36fb27a1516f9ad2d53bf7043aa56e7dfcf0204d
Daniel Tran <danieltran354@gmail.com>
2025-09-22 10:32:08 +0200

    wrote a comment to show which database format to use

--- includes/database.php
+++ includes/database.php
@@ -5,6 +5,8 @@ $username   = 'root';
 $password   = '';
 $database   = 'prototype_nutribot';
 
+// 	utf8mb4_0900_ai_ci GEBRUIK DEZE FORMAT OM DE DATABASE TE MAKEN
+
 $db = mysqli_connect($host, $username, $password, $database)
 or die('Error: '.mysqli_connect_error());
 ?>
