f605f021c85a336649d2a2c002ad4255d58ce38a
Thomasplie <1039350@hr.nl>
2025-09-22 14:54:47 +0200

    Trying to get json

--- index.php
+++ index.php
@@ -1,1 +1,61 @@
 <?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">-->
+    <link href="src/output.css" rel="stylesheet">
+    <title>Nutricoach</title>
+</head>
+<body>
+
+<!--<nav>-->
+<!---->
+<!--    <section>-->
+<!--        <div>-->
+<!--            <p class="font-alan text-8xl">-->
+<!--                Dis is A test :D-->
+<!--            </p>-->
+<!--        </div>-->
+<!--    </section>-->
+<!---->
+<!--</nav>-->
+
+<header>
+
+    <section>
+        <div>
+            <!--            In de header komt het icoon wat een button is, daarnaast hebben we rechts 2 buttons zitten.-->
+            <!--            Settings & Notificaties. -->
+        </div>
+    </section>
+
+</header>
+
+<main>
+
+    <section>
+        <!--        In de main komt de chat met de chatbot, Aan het begin zal er een bericht staan van; "Vul een prompt in-->
+        <!--        zodat ik je kan helpen! En als er eenmaal iets is gegeven is het gewoon een chat.-->
+        <!--        Maak van Settings een pop up slide van de zijkant, zo kan je prgm 3 gebruiken.-->
+    </section>
+
+</main>
+
+<footer>
+
+    <section>
+        <!--        In de footer komt er een send knop en een chat vak waar je prompts in kan vullen. -->
+    </section>
+
+</footer>
+
+</body>
+</html>
+
+
